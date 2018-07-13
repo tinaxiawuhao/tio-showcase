6f270054e06ce7e3f8039dac4323e03fd8f52f41
talent-tan <tywo45@163.com>
2018-07-13 17:41:28 +0800

    no commit message

--- im/client/installAndCopy.bat
+++ im/client/installAndCopy.bat
@@ -1,5 +1,5 @@
 call mvn clean install
 
 call rd ..\..\..\..\dist\examples\im\client /s /q
-call xcopy target\dist\tio-examples-im-client-3.1.0.v20180705-RELEASE ..\..\..\..\dist\examples\im\client\ /s /e /q /y
+call xcopy target\dist\tio-examples-im-client-3.1.2.v20180712-RELEASE ..\..\..\..\dist\examples\im\client\ /s /e /q /y
 
